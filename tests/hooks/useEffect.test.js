42dfbe74b7c8b288805f5cbf4a5ce11ae95e5241
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2020-09-13 16:51:01 +1000

    compile cleanup functions (to onDestroy)

--- tests/hooks/useEffect.test.js
+++ tests/hooks/useEffect.test.js
@@ -62,4 +62,72 @@ describe('calls to useEffect are compiled properly', () => {
       );
     });
   });
+
+  describe('useEffect callbacks with return values (onDestroy)', () => {
+    const jsx = `
+      import React from 'react';
+  
+      export default ({ prop }) => {
+        useEffect(() => { work(); return () => cleanup() }, [prop]);
+        useEffect(() => { mountWork(); return () => moreCleanup() }, []);
+        useEffect(() => () => extremelyClean());
+        
+        // empty callback bodies
+        useEffect(() => { return () => cleanup() }, [prop]);
+        useEffect(() => { return () => moreCleanup() }, []);
+
+  
+        return <div>testing afterUpdate</div>;
+      };
+    `;
+
+    const compiledCode = jsx2svelte.compile(jsx);
+    test('imports afterUpdate from svelte', () => {
+      expect(compiledCode).toContain(
+        'import { onDestroy, afterUpdate, onMount } from "svelte"'
+      );
+    });
+
+    const codeWithoutSpace = utils.removeWhiteSpace(compiledCode);
+    test('non-empty array, with return statement', () => {
+      expect(codeWithoutSpace).toContain(
+        utils.removeWhiteSpace('onDestroy(() => cleanup())')
+      );
+
+      expect(codeWithoutSpace).toContain(
+        utils.removeWhiteSpace('afterUpdate(() => { work(); })')
+      );
+    });
+
+    test('empty array, with return statement', () => {
+      expect(codeWithoutSpace).toContain(
+        utils.removeWhiteSpace('onDestroy(() => moreCleanup())')
+      );
+      expect(codeWithoutSpace).toContain(
+        utils.removeWhiteSpace('onMount(() => { mountWork(); })')
+      );
+    });
+
+    test('no array, no body', () => {
+      expect(codeWithoutSpace).toContain(
+        utils.removeWhiteSpace('onDestroy(() => extremelyClean())')
+      );
+    });
+
+    test.skip('lifecycle calls should not have no-op callbacks', () => {
+      expect(codeWithoutSpace).not.toContain(
+        utils.removeWhiteSpace('afterUpdate(() => {})')
+      );
+      expect(codeWithoutSpace).not.toContain(
+        utils.removeWhiteSpace('onDestroy(() => {})')
+      );
+      expect(codeWithoutSpace).not.toContain(
+        utils.removeWhiteSpace('onMount(() => {})')
+      );
+    });
+
+    test('the call to useEffect should be removed', () => {
+      expect(compiledCode).not.toContain('useEffect');
+    });
+  });
 });
